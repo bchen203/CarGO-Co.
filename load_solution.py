bd3db96bb26305a84bda072feb23e49de9392d4e
SquidShar <93841203+SquidShar@users.noreply.github.com>
2024-11-15 20:45:34 +0000

    added function to add containers to list

---
+++ load_solution.py
@@ -0,0 +1,17 @@
+#import manifest.py
+
+
+class Loader:
+    def _init_(self,uploaded_manifest)
+    self.manifest = uploaded_manifest
+    self.load_list = []
+
+    #adds a container from a truck into the load list
+    def add_load(name,weight)
+        truck_container = Container(name,weight)
+        load_list.append(truck_container)
+
+    
+    
+
+
