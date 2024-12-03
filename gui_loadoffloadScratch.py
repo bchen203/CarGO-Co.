6a0bf8e01a677cb8745cf3cfbf6db4ba542665d1
JakeBlack2003 <78673497+JakeBlack2003@users.noreply.github.com>
2024-12-03 01:03:49 -0800

    added load button and pending load/offload buttons

--- gui_loadoffloadScratch.py
+++ gui_loadoffloadScratch.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import simpledialog
+from tkinter import messagebox
 import manifest
 
 class GUI:
@@ -13,7 +14,8 @@ class GUI:
         self.currUser = "User"
         self.frames = [] # store all pages of gui to display
 
-        self.selectOperation()
+        self.operation = "load"
+        self.loadManifest()
 
     def menuBar(self):
         self.userMenu = Menubutton(self.master, text=self.currUser, bd=0)
@@ -132,6 +134,8 @@ class GUI:
                 self.manifest_upload.place(relwidth=1, relheight=1)
                 self.menuBar()
 
+                
+
             else: # button to calculate solution for balance operation
                 self.calculate_button_border.place(relx=0.975, rely=0.975, relheight=0.1, relwidth=0.15, anchor="se")
                 self.calculate_button.place(relheight=1, relwidth=1)
@@ -204,6 +208,7 @@ class GUI:
 
         self.manifest_display.place(relx=0.975, rely=0.5, relwidth=0.75, relheight=0.75, anchor="e")
         self.manifest_label.place(relx=0.5, rely=0.075, anchor="center")
+        self.renderLoadOffloadButtons()
         self.container_select.place(relwidth=1, relheight=1)
         self.menuBar()
 
@@ -219,18 +224,150 @@ class GUI:
         else:
             self.containers[y][x].configure(background="red", activebackground="green")
             self.offload_list.update({description: curr_offload - 1}) # decrease selected offload by 1
-
+        
+        self.updatePendingOffloads()
         print(self.offload_list)
 
 
+    def renderLoadOffloadButtons(self):
+        # helper function to define and place the buttons that will be used during load/offload operations
+        self.load_container_button_border = Frame(self.container_select,
+                                                   highlightbackground="black",
+                                                   background="black",
+                                                   bd=4)
+        self.load_container_button = Button(self.load_container_button_border,
+                                             background="#00ff14",
+                                             text = "+\nLoad",
+                                             command=self.loadContainerPrompt,
+                                             font=("Arial", 16, "bold"),
+                                             relief="flat",
+                                             borderwidth=0,
+                                             activebackground="#00CD14")
+        
+        self.pending_loads_button_border = Frame(self.container_select,
+                                                   highlightbackground="black",
+                                                   background="black",
+                                                   bd=4)
+        self.pending_loads_button = Button(self.pending_loads_button_border,
+                                             text = "0 pending loads",
+                                             command=self.displayPendingLoadsList,
+                                             font=("Arial", 14, "bold"),
+                                             relief="flat",
+                                             borderwidth=0,
+                                             activebackground="#DBDBDB")
+        
+        self.pending_offloads_button_border = Frame(self.container_select,
+                                                   highlightbackground="black",
+                                                   background="black",
+                                                   bd=4)
+        self.pending_offloads_button = Button(self.pending_offloads_button_border,
+                                             text = "0 pending offloads",
+                                             #command=self.displayPendingOffloadsList,
+                                             font=("Arial", 14, "bold"),
+                                             relief="flat",
+                                             borderwidth=0,
+                                             activebackground="#DBDBDB")
+
+        self.load_container_button_border.place(relx=0.1, rely=0.2, relheight=0.1, relwidth=0.1, anchor="nw")
+        self.load_container_button.place(relheight=1, relwidth=1)
+        self.pending_loads_button_border.place(relx=0.02, rely=0.32, relheight=0.05, relwidth=0.18, anchor="nw")
+        self.pending_loads_button.place(relheight=1, relwidth=1)
+        self.pending_offloads_button_border.place(relx=0.02, rely=0.39, relheight=0.05, relwidth=0.18, anchor="nw")
+        self.pending_offloads_button.place(relheight=1, relwidth=1)
+
+    def updatePendingLoads(self):
+        pendingLoadsNum = sum(self.load_list.values())
+        pendingLoadsText = "0 pending loads"
+
+        if(pendingLoadsNum == 1):
+            pendingLoadsText = f"{pendingLoadsNum} pending load"
+        else:
+            pendingLoadsText = f"{pendingLoadsNum} pending loads"
+
+        self.pending_loads_button.config(text = pendingLoadsText)
+
+    def updatePendingOffloads(self):
+        pendingOffloadsNum = sum(self.offload_list.values())
+        pendingOffloadsText = "0 pending offloads"
+
+        if(pendingOffloadsNum == 1):
+            pendingOffloadsText = f"{pendingOffloadsNum} pending offload"
+        else:
+            pendingOffloadsText = f"{pendingOffloadsNum} pending offloads"
+
+        self.pending_offloads_button.config(text = pendingOffloadsText)
+
+    def loadContainerPrompt(self):
+        loadContainerDescription = simpledialog.askstring(title="Load Container", prompt="Please enter a description for the container")
+        while(loadContainerDescription is not None and (loadContainerDescription.strip() == "" or loadContainerDescription.strip() == "UNUSED" or loadContainerDescription.strip() == "NAN")):
+            messagebox.showerror("Error", f"\"{loadContainerDescription}\" is an invalid container description")
+            loadContainerDescription = simpledialog.askstring(title="Load Container", prompt="Please enter a description for the container")
+        if loadContainerDescription is not None:
+            if loadContainerDescription in self.load_list:
+                curr_load_dupe = self.load_list.get(loadContainerDescription)
+                self.load_list.update({loadContainerDescription: curr_load_dupe + 1}) # increase selected load by 1
+            else:
+                self.load_list[f"{loadContainerDescription}"] = 1
+            self.updatePendingLoads()
+            print(self.load_list)
+
+    def displayPendingLoadsList(self):
+        self.pendingLoadsWindow = Toplevel()
+        self.pendingLoadsWindow.grab_set() # ensure that other windows cannot be modified while this one is open
+        self.pendingLoadsWindow.geometry("200x300")
+        self.pendingLoadsWindow.title("Pending Loads")
+        self.pending_loads_listbox = Listbox(self.pendingLoadsWindow,
+                          activestyle="dotbox",
+                          selectmode="multiple",
+                          selectbackground="red")
+        for containerDescription in self.load_list:
+            for dupes in range(self.load_list.get(containerDescription)):
+                self.pending_loads_listbox.insert(END, containerDescription)
+        
+        self.pending_loads_delete_button_border = Frame(self.pendingLoadsWindow,
+                                                   highlightbackground="black",
+                                                   background="black",
+                                                   bd=2)
+        self.pending_loads_delete_button = Button(self.pending_loads_delete_button_border,
+                                             background="red",
+                                             text = "Delete",
+                                             command=self.deletePendingLoads,
+                                             font=("Arial", 12, "bold"),
+                                             relief="flat",
+                                             borderwidth=0,
+                                             activebackground="#C90000")
+
+        self.pending_loads_listbox.place(relx=0.5, rely=0.05, relheight=0.8, relwidth=0.6, anchor="n")
+        self.pending_loads_delete_button_border.place(relx=0.5, rely=0.95, relheight=0.095, relwidth=0.3, anchor="s")
+        self.pending_loads_delete_button.place(relheight=1, relwidth=1)
+        self.pendingLoadsWindow.mainloop()
+
+    def deletePendingLoads(self):
+        #print(self.pending_loads_listbox.curselection())
+        for delIndex in sorted(self.pending_loads_listbox.curselection(), reverse=TRUE):
+            #print(f"deleteing index {delIndex}")
+            container2DeleteDescription = self.pending_loads_listbox.get(delIndex)
+            #print(container2DeleteDescription)
+            # remove container description from listbox
+            self.pending_loads_listbox.delete(delIndex)
+            # decrement the number of dupes of the container description from dict. if there are no more dupes, remove the container description entirely 
+            if container2DeleteDescription in self.load_list:
+                curr_load_dupe = self.load_list.get(container2DeleteDescription)
+                if(curr_load_dupe - 1 == 0):
+                    del self.load_list[container2DeleteDescription]
+                else:
+                    self.load_list.update({container2DeleteDescription: curr_load_dupe - 1}) # increase selected load by 1
+        self.updatePendingLoads()
+
+    #def displayPendingOffloadsList(self):
 
 
 
     def loadManifest(self):
         # deload operation selection screen
-        self.operation_select.place_forget()
-        self.userMenu.place_forget()
-        self.logMenu.place_forget()
+        #self.operation_select.place_forget()
+        #self.userMenu.place_forget()
+        #self.logMenu.place_forget()
 
         # configure manifest upload screen
         self.manifest_upload = Frame(self.master)
@@ -282,3 +419,22 @@ class GUI:
         # redraw menubar over manifest_upload screen
         self.menuBar()
         self.select_manifest_file()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    #import gui
+    import tkinter as tk
+
+    window = tk.Tk()
+    window.title("CargoCo Ship Optimizer")
+    GUI(window)
+    window.mainloop()
